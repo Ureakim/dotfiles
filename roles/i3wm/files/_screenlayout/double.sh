85fddf809820680377c5002a42e1981d3b9b4fba
Ureakim <dev@ureakim.net>
2025-02-01 21:41:04 +0100

    add screen layouts

--- roles/i3wm/files/_screenlayout/double.sh
+++ roles/i3wm/files/_screenlayout/double.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --primary --mode 3440x1440 --pos 1920x0 --rotate normal
+xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 3440x1440 --pos 1920x0 --rotate normal
--- roles/i3wm/files/_screenlayout/single.sh
+++ roles/i3wm/files/_screenlayout/single.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal
+xrandr --output DisplayPort-0 --off --output HDMI-A-0 --primary --mode 3440x1440 --pos 0x0 --rotate normal
